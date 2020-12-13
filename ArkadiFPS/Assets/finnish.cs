391e881957a263b07826e6c3bf6236e61336bc17
SamsungiphonePro <73790749+SamsungiphonePro@users.noreply.github.com>
2020-12-13 14:50:47 -0800

    versionn2

---
+++ ArkadiFPS/Assets/finnish.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class finnish : MonoBehaviour
+{
+    public Transform target;
+    public Transform player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject.Find("kagus").GetComponent<Text>().text = Distance().ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GameObject.Find("kagus").GetComponent<Text>().text = Distance().ToString();
+    }
+
+    private float Distance()
+    {
+        return Vector3.Distance(target.position, player.position);
+    }
+}
